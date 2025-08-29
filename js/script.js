e90caabb8ffa99186af4e3919a7e5c8e6119c2bc
faizah qatrunnada <faizahqatrunnada10@gmail.com>
2025-08-30 02:51:14 +0700

    Controls the mobile hamburger menu.

--- js/script.js
+++ js/script.js
@@ -9,3 +9,13 @@ document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('user-greeting').textContent = userName;
   document.getElementById('main-content').classList.remove('hidden');
 });
+
+// =======================
+// Toggle menu (hamburger)
+// =======================
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+  menuBtn.textContent = menu.classList.contains('hidden') ? '☰' : '✕';
+});
